cb4c2009cd8ce977d045edec4fe91afdc98f4d56
papiroca-tm <danshin.workspace@gmail.com>
2017-04-09 22:47:16 +0300

    init smartAdmin frontend app engine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, ViewContainerRef} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<router-outlet></router-outlet>'
 })
 export class AppComponent {
-  title = 'app works!!!';
+  public title = 'app works!';
+
+  public constructor(private viewContainerRef: ViewContainerRef) {}
+
 }
